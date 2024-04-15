92d4e7bd2e3458b6955fca4ce8e43a5c591a996c
ShoyimObloqulov <obloqulovshoyim1@gmail.com>
2024-04-15 12:36:34 +0500

    index page and tkinter and pygame module add

--- index.py
+++ index.py
@@ -1,6 +1,22 @@
 from tkinter import *
-root = TK()
+import pygame
+root = Tk()
 root.title('JALOLOVA')
-root.iconbitmap('music.png')
+root.iconbitmap('music.ico')
 root.geometry("500x400")
+pygame.mixer.init()
+
+def play():
+    pygame.mixer.music.load("audio/bass.mp3")
+    pygame.mixer.music.play(loops=0)
+def stop():
+    pygame.mixer.music.stop()
+
+my_button = Button(root,text="Eshitish",font=("Helvetica",14),command=play)
+my_button.pack(pady=20)
+
+stop_button = Button(root,text="To'xtatish",font=("Helvetica",14),command=stop)
+stop_button.pack()
+
 root.mainloop()
+
